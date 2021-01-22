ce48d31dbbef8c19961cd604c5e061693d4d70c7
Nielsen <UM-AD\rvnhmq@rc05xcs213.managed.mst.edu>
2021-01-22 15:30:42 -0600

    splitting functions for readability

--- python/thesaurusize/thesaurusize.py
+++ python/thesaurusize/thesaurusize.py
@@ -5,6 +5,42 @@ from random import seed
 from random import randint
 import time
 
+def fix_spaces(synonym):
+      # temporary synonym and counter for fixing spaces
+      temp_syn = ""
+      t = 0
+      while(t < len(synonym)):
+            if(synonym[t] == '%' and synonym[t + 1] == '2' and synonym[t + 2] == '0'):
+                  temp_syn += ' '
+                  t += 2
+            else:
+                  temp_syn = temp_syn + synonym[t]
+            t += 1
+      # change synonym to the version with correct spaces and return
+      synonym = temp_syn
+      return synonym
+
+def get_syn(linestring, y, synonym):
+      # create the synonym string and stop when quotes are reached
+      i = 0
+      while True:
+            # stop at the end of the word
+            if(linestring[y + i] == "\""):
+                  return fix_spaces(synonym)
+            synonym = synonym + linestring[y + i]
+            i += 1
+
+def find_browse(linestring, syn_num):
+      rand_num = randint(2, 5)
+      for y in range(len(linestring)):
+            synonym = ""
+            # search for the end of the word "browse" and when found, check if this is the right synonym to use
+            if(linestring[y] == 'w' and linestring[y + 1] == 's' and linestring[y + 2] == 'e'):
+                  if(syn_num == rand_num):
+                        y += 4
+                        return get_syn(linestring, y, synonym)
+                  syn_num += 1
+
 def thesaurusize(word):
       # open the website with the word as the address and write to file "test.txt"
       address = "https://www.thesaurus.com/browse/" + word + "?s=t"
@@ -21,53 +57,62 @@ def thesaurusize(word):
       # open the file with the thesaurus page and go through each line
       with open('thesaurus_page.txt') as f:
             for line in f:
-                  # which synonym on the page is being used 
-                  rand_num = randint(2, 5)
                   # when the line with the synonyms is reached, index through the line as a string
                   if(syn_line == 136):
-                        linestring = str(line)
-                        for y in range(len(linestring)):
-                              synonym = ""
-                              # search for the end of the word "browse" and when found, check if this is the right synonym to use
-                              if(linestring[y] == 'w' and linestring[y + 1] == 's' and linestring[y + 2] == 'e'):
-                                    if(syn_num == rand_num):
-                                          y += 4
-                                          # create the synonym string and stop when anything other than a letter is reached
-                                          i = 0
-                                          while True:
-                                                # stop at the end of the word
-                                                if(linestring[y + i] == "\""):
-                                                      # temporary synonym and counter for fixing spaces
-                                                      temp_syn = ""
-                                                      t = 0
-                                                      while(t < len(synonym)):
-                                                            if(synonym[t] == '%' and synonym[t + 1] == '2' and synonym[t + 2] == '0'):
-                                                                  temp_syn += ' '
-                                                                  t += 2
-                                                            else:
-                                                                  temp_syn = temp_syn + synonym[t]
-                                                            t += 1
-                                                      # change synonym to the version with correct spaces and return
-                                                      synonym = temp_syn
-                                                      return synonym
-                                                synonym = synonym + linestring[y + i]
-                                                i += 1
-                                          break
-                                    syn_num += 1
+                        return find_browse(str(line), syn_num)
                   syn_line += 1
       # if for some reason, the function hasn't already returned, display error
       return "---ERROR: can't find synonym---"
+
+def fix_caps(found_capital, the_word):
+      case_word = ""
+      for h in range(len(the_word)):
+            case_word += chr(ord(the_word[h]) - 32 * found_capital)
+            found_capital = 0
+      return case_word
+
+def try_thes(new_paragraph, c_word, found_capital):
+      # try to search the thesarus for the word
+      while True:
+            # if it works, check if the case of the first letter needs to be raised
+            try:
+                  the_word = thesaurusize(c_word)
+                  if(found_capital == 1):
+                        the_word = fix_caps(found_capital, the_word)
+                        found_capital = 0
+                  # add the new word to the new paragraph
+                  new_paragraph += the_word + " "
+                  break
+            # if the thesarus couldn't find the word, leave it as is
+            except urllib.error.HTTPError:
+                  # add the original word to the new paragraph
+                  new_paragraph += c_word + " " # todo
+                  break
+      return new_paragraph
+
+def end_of_word(c_word, found_capital, new_paragraph):
+      # if the word is at least 4 letters long, start replacement process
+      if(len(c_word) > 3):
+            # # try to search the thesarus for the word
+            new_paragraph = try_thes(new_paragraph, c_word, found_capital)
+      else:
+            # reset the capital flag and add the current word since it is not
+                  # long enough to be replaced
+            new_paragraph += c_word + " " # todo
+      return new_paragraph
 
 def find_replacements(paragraph):
       # open the file to be editing and turn into a string
       my_file = open(paragraph, "r")
       paragraph_str = my_file.read()
+
       # string to store the paragraph with replaced words
       new_paragraph = ""
-      # is there a capital letter in the word
-      found_capital = 0
       # current word
       c_word = ""
+      # should the current word be capitalized
+      found_capital = 0
+
       # for the entire paragraph
       for u in range(len(paragraph_str)):
             # if there's a capital letter in a word, flag it
@@ -77,43 +122,49 @@ def find_replacements(paragraph):
             if((ord(paragraph_str[u]) < 97 or ord(paragraph_str[u]) > 122) and 
                   (ord(paragraph_str[u]) < 65 or ord(paragraph_str[u]) > 90) and (ord(paragraph_str[u]) < 48 or ord(paragraph_str[u]) > 57)):
                   # if the word is at least 4 letters long, start replacement process
-                  if(len(c_word) > 3):
-                        # try to search the thesarus for the word
-                        while True:
-                              # if it works, check if the case of the first letter needs to be raised
-                              try:
-                                    the_word = thesaurusize(c_word)
-                                    case_word = ""
-                                    if(found_capital == 1):
-                                          for h in range(len(the_word)):
-                                                case_word += chr(ord(the_word[h]) - 32 * found_capital)
-                                                found_capital = 0
-                                          the_word = case_word
-                                    # add the new word to the new paragraph
-                                    new_paragraph += the_word + " "
-                                    break
-                              # if the thesarus couldn't find the word, leave it as is
-                              except urllib.error.HTTPError:
-                                    # add the original word to the new paragraph
-                                    new_paragraph += c_word + " " # todo
-                                    break
-                  else:
-                        # reset the capital flag and add the current word since it is not
-                              # long enough to be replaced
-                        found_capital = 0
-                        new_paragraph += c_word + " " # todo
-                  # reset current word
+                  new_paragraph = end_of_word(c_word, found_capital, new_paragraph)
+                  # reset current word and capital counter
                   c_word = ""
+                  found_capital = 0
             else:
                   # add the current letter to the current word
                   c_word += paragraph_str[u]
             # if the current character is punctuation, remove space after the previous 
-                  # word and add the punctuation to the new paragraph
+            #     word and add the punctuation to the new paragraph
             if(ord(paragraph_str[u]) >= 33 and ord(paragraph_str[u]) <= 47):
                   new_paragraph = new_paragraph[:-1] + paragraph_str[u]
       # completed going through original paragraph, so new paragraph is complete
       my_file.close()
       return new_paragraph
+
+
+def find_indiv_syn(indiv_word):
+      while True:
+            try:
+                  return("\"Synonym\": " + thesaurusize(indiv_word))
+                  break
+            except urllib.error.HTTPError: 
+                  return("No synonyms found")
+                  break
+
+def space_replace(indiv_word):
+      # replace any spaces with %20 for the website address
+      no_space_word = ""
+      for h in range(len(indiv_word)):
+            if(indiv_word[h] == ' '):
+                  no_space_word += "%20"
+            else:
+                  no_space_word += indiv_word[h]
+      return no_space_word
+
+def word_mode():
+      indiv_word = 'a'
+      # get user inputted words and find synonyms until they quit
+      while(indiv_word != 'q'):
+            indiv_word = input("Enter a word. (wite 'q' to quit): ")
+            print("Your word: " + indiv_word)
+            indiv_word = space_replace(indiv_word)
+            return find_indiv_syn(indiv_word)
 
 def main():
       # try to open user inputted file and ask for another name if not available
@@ -149,25 +200,6 @@ def main():
                         new_file.close()
       # individual word mode
       elif(mode == 'w'):
-            indiv_word = 'a'
-            # get user inputted words and find synonyms until they quit
-            while(indiv_word != 'q'):
-                  indiv_word = input("Enter a word. (wite 'q' to quit): ")
-                  # replace any spaces with %20 for the website address
-                  no_space_word = ""
-                  for h in range(len(indiv_word)):
-                        if(indiv_word[h] == ' '):
-                              no_space_word += "%20"
-                        else:
-                              no_space_word += indiv_word[h]
-                  print("Your word: " + indiv_word)
-                  indiv_word = no_space_word
-                  while True:
-                        try:
-                              print("\"Synonym\": " + thesaurusize(indiv_word))
-                              break
-                        except urllib.error.HTTPError: 
-                              print("No synonyms found")
-                              break
+            print(word_mode())
 
 main()
